b9b2f7cd4798167d0a1b014513d63b5705105158
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-10-28 11:13:28 +0100

    Added spinner using spinkit package

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
-
 import 'package:time_flip/service/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -32,12 +30,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Text(
-          'loading'
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 50.0,
         ),
-      ),
+      )
     );
   }
 }
