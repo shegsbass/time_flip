ff8023939efbc1ef6f2b5d7905a708157b7b0ed1
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-10-28 10:59:04 +0100

    Time formatted

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -24,18 +24,45 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            ElevatedButton.icon(
-                onPressed: (){
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: const Icon(Icons.edit_location),
-                label: const Text(
-                  'Edit Location'
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+          child: Column(
+            children: [
+              ElevatedButton.icon(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: const Icon(Icons.edit_location),
+                  label: const Text(
+                    'Edit Location'
+                  ),
+              ),
+
+              SizedBox(height: 20.0),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                        data['location'],
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          letterSpacing: 2.0,
+                        ),
+                    ),
+                    SizedBox(height: 20.0),
+
+                    Text(
+                        data['time'],
+                        style: TextStyle(
+                          fontSize: 66.0,
+                        ),
+                    ),
+                  ],
                 ),
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
