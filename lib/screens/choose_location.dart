518e87271ce0702aeeb00010ffa326784d4fd1b9
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-10-29 22:46:32 +0100

    Created list of locations

--- lib/screens/choose_location.dart
+++ lib/screens/choose_location.dart
@@ -1,6 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import '../service/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
@@ -11,22 +10,23 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
+  //Creating a list of different locations from the Worldtime class
+  List<WorldTime> locations = [
+    WorldTime(location: 'Nairobi, Kenya', flag: 'kenya.png', url: 'Africa/Nairobi'),
+    WorldTime(location: 'Cairo, Egypt', flag: 'egypt.png', url: 'Africa/Cairo'),
+    WorldTime(location: 'Johannesburg, SA', flag: 'sa.png', url: 'Africa/Johannesburg'),
+    WorldTime(location: 'Casablanca, Morroco', flag: 'morroco.png', url: 'Africa/Casablanca'),
+    WorldTime(location: 'Monrovia, Liberia', flag: 'liberia.png', url: 'Africa/Monrovia'),
+    WorldTime(location: 'Abidjan, Cote dIvoire', flag: 'cote.png', url: 'Africa/Abidjan'),
+    WorldTime(location: 'Juba, South Sudan', flag: 'sudan.png', url: 'Africa/Juba'),
+    WorldTime(location: 'Windhoek, Namibia', flag: 'namibia.png', url: 'Africa/Windhoek'),
+    WorldTime(location: 'Tunis, Tunisia', flag: 'tunisia.png', url: 'Africa/Tunis'),
+  ];
 
-    var url = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
-
-    Response response = await get(url);
-
-    Map data = jsonDecode(response.body);
-    print (data);
-    print(data['title']);
-
-  }
 
   @override
   void initState() {
     super.initState();
-    getData();
   }
 
   @override
@@ -35,9 +35,26 @@ class _ChooseLocationState extends State<ChooseLocation> {
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
-        title: Text('Choose a Location'),
+        title: const Text('Choose a Location'),
         centerTitle: true,
         elevation: 0.0,
+      ),
+      body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index){
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+              child: Card(
+                child: ListTile(
+                  onTap: (){},
+                  title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                  ),
+                ),
+              ),
+            );
+          }
       ),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -85,7 +85,7 @@ class _HomeState extends State<Home> {
                       ),
                     ],
                   ),
-                )
+                ),
               ],
             ),
           ),
